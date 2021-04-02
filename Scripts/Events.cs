c4feaf62145af28258c258abb191f0bc83e8d43b
AknDnm <79793127+AknDnm@users.noreply.github.com>
2021-04-02 12:34:58 +0300

    Delete Scripts directory

--- Scripts/Events.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine.Events;
-
-public static class Events
-{
-    public static UnityEvent StopRunning = new UnityEvent();
-    public static UnityEvent StartRunning = new UnityEvent();
-}
